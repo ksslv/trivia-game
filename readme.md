b1149385308021e2063b3b74cf9be6efa9dc3e3a
ksslv <kseniasalova+github@gmail.com>
2020-11-09 00:36:08 -0800

    Added proper fetching of data from the included JSON file; README update

--- readme.md
+++ readme.md
@@ -12,12 +12,14 @@ Assumptions:
 Features:
 
 - User can view questions
-- Questions + answer options are displayed one at a time
-- Questions are randomized and do not repeat in a single round
+- Questions + its answer options are displayed one at a time
+- Questions are randomized using a custom `shuffle` function and do not repeat in a single round
+- Correct + incorrect answers are also shuffled for each question
 - User can select only ONE answer option out of 4 (or less) possible answers
+- The edge case where less than 4 answers options are available is handled
 - The correct answer is revealed if the user's selected answer is incorrect
-- Correct + incorrect answers are shuffled for each question
-- User can see the score they received at the end of the round
+- User's score is tracked throughout the game and is updated when a new question is revealed
+- User can see their final score at the end of the round
 - User can choose to play another round when the game is finished
 
 ### Technologies used
@@ -29,12 +31,13 @@ HTML/CSS, Javscript, lite-server package, Jest for testing [TODO]
 From within the project directory, run in terminal:
 
 ```bash
+npm install
 npm run dev
 ```
 
 ### TODOs
 
 - [ ] refactoring
-- [ ] proper JSON fetching
+- [x] proper JSON fetching
 - [ ] unit tests
 - [ ] CSS/HTML clean-up
--- trivia.js
+++ trivia.js
@@ -1,118 +1,23 @@
-// TEMP: load data manually
-// TODO: enable fetching
-const data = [
-  {
-    question: "What was Tandem previous name?",
-    incorrect: ["Tandem", "Burger Shack", "Extraordinary Humans"],
-    correct: "Devmynd",
-  },
-  {
-    question: "In Shakespeare's play Julius Caesar, Caesar's last words were...",
-    incorrect: ["Iacta alea est!", "Vidi, vini, vici", "Aegri somnia vana"],
-    correct: "Et tu, Brute?",
-  },
-  {
-    question: "A group of tigers are referred to as:",
-    incorrect: ["Chowder", "Pride", "Destruction"],
-    correct: "Ambush",
-  },
-  {
-    question: "What is the top speed an average cat can travel?",
-    incorrect: ["42 mph", "13 mph", "9 mph"],
-    correct: "31 mph",
-  },
-  {
-    question: "A cat can jump to _____ times its own height:",
-    incorrect: ["3", "9", "7"],
-    correct: "5",
-  },
-  {
-    question: "What is the only letter that doesn't appear in a US state name?",
-    incorrect: ["M", "Z", "X"],
-    correct: "Q",
-  },
-  {
-    question: "What is the name for a cow-bison hybrid?",
-    incorrect: ["Cowson", "Bicow", "Mooson"],
-    correct: "Beefalo",
-  },
-  {
-    question: "What is the largest freshwater lake in the world?",
-    incorrect: ["Lake Baikal", "Lake Michigan", "Lake Victoria"],
-    correct: "Lake Superior",
-  },
+// fetch data from provided json and start the game
+fetch("data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    const data = json;
 
-  {
-    question: "In a website address bar, what does WWW stand for?",
-    incorrect: ["Wild Wild West", "War World Web"],
-    correct: "World Wide Web",
-  },
-  {
-    question: "In a game of bingo, what number is represented by the name two little ducks?",
-    incorrect: ["20", "55", "77"],
-    correct: "22",
-  },
-  {
-    question: "According to Greek mythology, who was the first woman on Earth?",
-    incorrect: ["Lilith", "Eve", "Hera"],
-    correct: "Pandora",
-  },
-  {
-    question: "In which European city would you find Orly airport?",
-    incorrect: ["London", "Belgium", "Munich"],
-    correct: "Paris",
-  },
-  {
-    question: "Where would you find the Sea of Tranquility?",
-    incorrect: ["California", "Siberia", "China"],
-    correct: "The Moon",
-  },
-  {
-    question: "Which artist painted 'Girl with a Pearl Earrin'?",
-    incorrect: ["Van Gogh", "Picasso", "Da Vinci"],
-    correct: "Vermeer",
-  },
-  {
-    question: "What is the official name for the 'hashtag' symbol?",
-    incorrect: ["Number sign", "Hash Sign", "Pound"],
-    correct: "Octothorpe",
-  },
-  {
-    question: "Not American at all, where is apple pie from?",
-    incorrect: ["Japan", "Ethiopia", "Canada"],
-    correct: "England",
-  },
-  {
-    question: "What is the national animal of Scotland?",
-    incorrect: ["Bear", "Rabbit", "Seal"],
-    correct: "Unicorn",
-  },
-  {
-    question: "Where in the world is the only place where Canada is *due south*",
-    incorrect: ["Alaska", "Russia", "Washington"],
-    correct: "Detroit",
-  },
-  {
-    question: "Approximately how many grapes go into a bottle of wine?",
-    incorrect: ["500", "200", "1000"],
-    correct: "700",
-  },
-  {
-    question: "How much does a US One Dollar Bill cost to make?",
-    incorrect: ["$0.25", "$1", "$5"],
-    correct: "$0.05",
-  },
-  {
-    question: "The Vatican bank has the only ATM in the world that allows users to do what?",
-    incorrect: ["Receive withdrawls in rosary beads", "Vote for the Pope", "Purchase indulgences"],
-    correct: "Perform transactions in Latin",
-  },
-];
+    nextButton.addEventListener("click", () => getQuestion(data));
+    newGameButton.addEventListener("click", () => playGame(data));
+
+    playGame(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 // set constants
 const roundLength = 10;
 const numberOfAnswers = 4;
-const maxJSONLength = data.length;
 
 // select DOM elements
 const questionEl = document.querySelector("#question");
@@ -125,13 +30,19 @@ const newGameButton = document.querySelector(".play-new-btn");
 
 const nextButton = document.querySelector(".next-btn");
 
+// initiatlize variables
+let clicked = false;
+let questionCounter = 0;
+let scoreSoFar = 0;
+let currCorrectAns;
+let selectedQuestionIdxs = [];
+
 /**
- * @desc randomly shuffles numbers from 0 to N-1 and from that the shuffled numbers
- *          chooses the subset of size numToChoose
+ * @desc picks `numToChoose`-many non-duplicate randomly generated integers and saves them
  * @param number - N, numToChoose
  * @return array - a shuffled array of numbers
  */
-function shuffle(N, numToChoose) {
+const shuffle = (N, numToChoose) => {
   let seenNums = new Set();
   let shuffledQuestionsIdx = [];
   let i = 0;
@@ -150,40 +61,42 @@ function shuffle(N, numToChoose) {
     }
   }
   return shuffledQuestionsIdx;
-}
+};
 
 /**
  * @desc starts a fresh trivia game with a new set of questions
  */
-playGame = () => {
+const playGame = (data) => {
+  const maxJSONLength = data.length;
   questionCounter = 0;
   scoreSoFar = 0;
   modal.style.display = "none";
 
   selectedQuestionIdxs = shuffle(maxJSONLength, roundLength);
 
-  getQuestion();
+  getQuestion(data);
 };
 
 /**
  * @desc fills out the Q and A DOM elements with loaded JSON data
  */
-getQuestion = () => {
+const getQuestion = (data) => {
   if (questionCounter < roundLength) {
-    currQuestionIdx = selectedQuestionIdxs[questionCounter];
-    currQuestion = data[currQuestionIdx];
+    let currQuestionIdx = selectedQuestionIdxs[questionCounter];
+    let currQuestion = data[currQuestionIdx];
 
     // 1. update the Q text
     question.innerText = currQuestion.question;
 
     // 2. prepare the answers
     currCorrectAns = currQuestion.correct;
-    currAllAnswers = [...currQuestion.incorrect, currCorrectAns];
+    let currAllAnswers = [...currQuestion.incorrect, currCorrectAns];
 
-    shuffledAnsIdxs = shuffle(currAllAnswers.length, currAllAnswers.length);
+    let shuffledAnsIdxs = shuffle(currAllAnswers.length, currAllAnswers.length);
 
-    answerCounter = -1;
+    let answerCounter = -1;
 
+    // update answer elements' text and their styles
     answerEls.forEach((answer) => {
       answer.parentElement.classList.remove("correct", "incorrect");
       answer.parentElement.style.display = "block";
@@ -214,13 +127,10 @@ getQuestion = () => {
   }
 };
 
-nextButton.addEventListener("click", getQuestion);
-newGameButton.addEventListener("click", playGame);
-
 /**
  * @desc handles click on an answer, both correct and incorrect
  */
-handleAnswerClick = (e) => {
+const handleAnswerClick = (e) => {
   if (clicked) return;
 
   clicked = true;
@@ -242,5 +152,3 @@ handleAnswerClick = (e) => {
     });
   }
 };
-
-playGame();
